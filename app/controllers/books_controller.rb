f317bdef8d913f1b80a1bf9c416065f16edd35ed
vagrant <vagrant@localhost.localdomain>
2020-06-02 13:07:58 +0000

    [Add]5-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,7 +20,7 @@ class BooksController < ApplicationController
 		@book.user_id = current_user.id
 		if @book.update(book_params)
 		   flash[:notice] = "Book was successfully updated."
-		   redirect_to books_path
+		   redirect_to book_path(@book.id)
 		else
 		   @books = Book.all
 		   render action: :edit
@@ -34,11 +34,11 @@ class BooksController < ApplicationController
 	end
 
 	def create
-		@book =Book.new(book_params)
+		@book = Book.new(book_params)
 		@book.user_id = current_user.id
 		if @book.save
 		   flash[:notice] = "Book was successfully created."
-		   redirect_to books_path
+		   redirect_to book_path(@book.id)
 		else
 		   @books = Book.all
 		   render action: :index
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,8 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
+    @book = Book.new
+    # @books = @user.books.all
   end
 
   def edit
@@ -14,13 +16,21 @@ class UsersController < ApplicationController
 
   def update
   	@user = User.find(params[:id])
-  	@user.update(user_params)
-  	redirect_to user_path(@user.id)
+    if @user.update(user_params)
+       flash[:notice] = "User was successfully updated."
+  	   redirect_to user_path(@user.id)
+    else
+       @users = User.all
+       render action: :edit
+    end
+
   end
 
   private
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
-
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
+  end
 end
